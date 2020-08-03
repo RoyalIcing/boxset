a519d15f63ad7c5ecf26c2002767c68d37380575
Patrick Smith <pgwsmith@gmail.com>
2020-08-03 17:52:26 +1000

    Add set operations

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,24 @@
-import { lookup, inverse, union } from './index';
+import {
+  emptySet,
+  universalSet,
+  lookup,
+  complement,
+  union,
+  difference,
+  intersection,
+} from './index';
+
+describe('emptySet', () => {
+  it('always returns false', () => {
+    expect(emptySet('anything')).toBe(false);
+  });
+});
+
+describe('universalSet', () => {
+  it('always returns true', () => {
+    expect(universalSet('anything')).toBe(true);
+  });
+});
 
 describe('lookup', () => {
   it('works with an Array', () => {
@@ -7,7 +27,12 @@ describe('lookup', () => {
     expect(get('first')).toBe(true);
     expect(get('second')).toBe(true);
     expect(get('missing')).toBe(false);
-    expect(new Map(get())).toEqual(new Map([['first', true], ['second', true]]));
+    expect(new Map(get())).toEqual(
+      new Map([
+        ['first', true],
+        ['second', true],
+      ])
+    );
   });
 
   it('works with a Set', () => {
@@ -45,14 +70,30 @@ describe('lookup', () => {
   });
 });
 
-describe('inverse', () => {
+describe('complement', () => {
   it('works with a Set', () => {
     const [get] = lookup(new Set(['first', 'second']));
-    const inverseGet = inverse(get);
+    const inverseGet = complement(get);
 
     expect(inverseGet('first')).toBe(false);
     expect(inverseGet('second')).toBe(false);
     expect(inverseGet('missing')).toBe(true);
+  });
+
+  it('works with empty set', () => {
+    expect(complement(emptySet)('anything')).toBe(true);
+  });
+
+  it('works with double complement of empty set', () => {
+    expect(complement(complement(emptySet))('anything')).toBe(false);
+  });
+
+  it('works with universal set', () => {
+    expect(complement(universalSet)('anything')).toBe(false);
+  });
+
+  it('works with double complement of universal set', () => {
+    expect(complement(complement(universalSet))('anything')).toBe(true);
   });
 });
 
@@ -67,11 +108,31 @@ describe('union', () => {
     expect(get('third')).toBe(true);
     expect(get('fourth')).toBe(true);
     expect(get('missing')).toBe(false);
+
+    expect(new Map(get())).toEqual(
+      new Map([
+        ['first', true],
+        ['second', true],
+        ['third', true],
+        ['fourth', true],
+      ])
+    );
   });
 
   it('works with two Maps', () => {
-    const [a] = lookup(new Map([['first', 1], ['second', 2], ['third', -3]]));
-    const [b] = lookup(new Map([['third', 3], ['fourth', 4]]));
+    const [a] = lookup(
+      new Map([
+        ['first', 1],
+        ['second', 2],
+        ['third', -3],
+      ])
+    );
+    const [b] = lookup(
+      new Map([
+        ['third', 3],
+        ['fourth', 4],
+      ])
+    );
     const get = union(a, b);
 
     expect(get('first')).toBe(1);
@@ -80,6 +141,125 @@ describe('union', () => {
     expect(get('fourth')).toBe(4);
     expect(get('missing')).toBe(undefined);
 
-    expect(new Map(get())).toEqual(new Map([['first', 1], ['second', 2], ['third', 3], ['fourth', 4]]));
+    expect(new Map(get())).toEqual(
+      new Map([
+        ['first', 1],
+        ['second', 2],
+        ['third', 3],
+        ['fourth', 4],
+      ])
+    );
   });
-})
+});
+
+describe('difference', () => {
+  it('works with an Array and Set', () => {
+    const [a] = lookup(['first', 'second', 'third']);
+    const [b] = lookup(new Set(['third', 'fourth']));
+    const get = difference(a, b);
+
+    expect(get('first')).toBe(true);
+    expect(get('second')).toBe(true);
+    expect(get('third')).toBe(false);
+    expect(get('fourth')).toBe(false);
+    expect(get('missing')).toBe(false);
+
+    expect(new Map(get())).toEqual(
+      new Map([
+        ['first', true],
+        ['second', true],
+      ])
+    );
+  });
+
+  it('works with two Maps', () => {
+    const [a] = lookup(
+      new Map([
+        ['first', 1],
+        ['second', 2],
+        ['third', -3],
+      ])
+    );
+    const [b] = lookup(
+      new Map([
+        ['third', 3],
+        ['fourth', 4],
+      ])
+    );
+    const get = difference(a, b);
+
+    expect(get('first')).toBe(1);
+    expect(get('second')).toBe(2);
+    expect(get('third')).toBe(undefined);
+    expect(get('fourth')).toBe(undefined);
+    expect(get('missing')).toBe(undefined);
+
+    expect(new Map(get())).toEqual(
+      new Map([
+        ['first', 1],
+        ['second', 2],
+      ])
+    );
+  });
+});
+
+describe('intersection', () => {
+  it('works with an Array and Set', () => {
+    const [a] = lookup(['first', 'second', 'third']);
+    const [b] = lookup(new Set(['third', 'fourth']));
+    const get = intersection(a, b);
+
+    expect(get('first')).toBe(false);
+    expect(get('second')).toBe(false);
+    expect(get('third')).toBe(true);
+    expect(get('fourth')).toBe(false);
+    expect(get('missing')).toBe(false);
+
+    expect(new Map(get())).toEqual(new Map([['third', true]]));
+  });
+
+  it('works with two Maps', () => {
+    const [a] = lookup(
+      new Map([
+        ['first', 1],
+        ['second', 2],
+        ['third', -3],
+      ])
+    );
+    const [b] = lookup(
+      new Map([
+        ['third', 3],
+        ['fourth', 4],
+      ])
+    );
+    const get = intersection(a, b);
+
+    expect(get('first')).toBe(undefined);
+    expect(get('second')).toBe(undefined);
+    expect(get('third')).toBe(-3);
+    expect(get('fourth')).toBe(undefined);
+    expect(get('missing')).toBe(undefined);
+
+    expect(new Map(get())).toEqual(new Map([['third', -3]]));
+  });
+
+  it('works with a Map and an array', () => {
+    const [a] = lookup(
+      new Map([
+        ['first', 1],
+        ['second', 2],
+        ['third', -3],
+      ])
+    );
+    const [b] = lookup(['third', 'fourth']);
+    const get = intersection(a, b);
+
+    expect(get('first')).toBe(undefined);
+    expect(get('second')).toBe(undefined);
+    expect(get('third')).toBe(-3);
+    expect(get('fourth')).toBe(undefined);
+    expect(get('missing')).toBe(undefined);
+
+    expect(new Map(get())).toEqual(new Map([['third', -3]]));
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-export type Getter<I, O> = (input: I) => O;
+export interface Getter<I, O> {
+  (input: I): O;
+}
 export type Contains<I> = Getter<I, boolean>;
-export interface GetterWithEntries<I, O> extends Getter<I, O> {
+export interface GetterWithEntries<I, O> {
+  (input: I): O;
+  (input: null): undefined | null | false;
   (): Iterable<[I, O]>;
 }
 
-export interface Inverted {
-  isInverted: true;
+export interface Complemented {
+  isComplement: true;
 }
+
+export const emptySet: Contains<string> = () => false;
+export const universalSet: Contains<string> = () => true;
 
 export function lookup<K, V>(
   source: ReadonlyMap<K, V>
@@ -18,9 +25,11 @@ export function lookup<T>(
 ): [GetterWithEntries<T, boolean>] {
   if (source instanceof Map) {
     const map = source;
-    const get = (input?: T) => {
+    const get = (input?: T | null) => {
       if (input === undefined) {
         return map.entries();
+      } else if (input === null) {
+        return undefined;
       } else {
         return map.get(input);
       }
@@ -37,13 +46,14 @@ export function lookup<T>(
           return {
             next() {
               const keyResult = iterator.next();
-              if (keyResult.done) return keyResult;
-              else {
-                return {
-                  value: [keyResult.value, true],
-                  done: false,
-                };
+              if (keyResult.done) {
+                return keyResult;
               }
+
+              return {
+                value: [keyResult.value, true],
+                done: false,
+              };
             },
           };
         },
@@ -53,6 +63,8 @@ export function lookup<T>(
 
       // })()
       // return store.entries();
+    } else if (input === null) {
+      return false;
     } else {
       return store.has(input);
     }
@@ -60,12 +72,14 @@ export function lookup<T>(
   return [get as GetterWithEntries<T, boolean>];
 }
 
-export function inverse<K>(source: Getter<K, any>): Contains<K> & Inverted {
+export function complement<K>(
+  source: Getter<K, any>
+): Contains<K> & Complemented {
   return Object.assign(
     (input: K) => {
       return !source(input);
     },
-    { isInverted: true as const }
+    { isComplement: true as const }
   );
 }
 
@@ -88,3 +102,84 @@ export function union<K, V>(
 
   return get as GetterWithEntries<K, V>;
 }
+
+export function difference<K, V>(
+  a: GetterWithEntries<K, V>,
+  b: GetterWithEntries<K, any>
+): GetterWithEntries<K, V> {
+  const get = (input?: K) => {
+    if (input === undefined) {
+      return function*() {
+        const iterator = a()[Symbol.iterator]();
+
+        while (true) {
+          let item = iterator.next();
+          while (!item.done && b(item.value[0])) {
+            item = iterator.next();
+          }
+
+          if (item.done) {
+            return;
+          }
+
+          yield item.value;
+        }
+      }();
+      // const result = new Map<K, V>(a());
+      // for (const [key] of Array.from(b())) {
+      //   result.delete(key);
+      // }
+      // return result;
+    } else {
+      const bResult = b(input);
+      if (bResult) {
+        return a(null);
+      }
+      return a(input);
+    }
+  };
+
+  return get as GetterWithEntries<K, V>;
+}
+
+export function intersection<K, V>(
+  a: GetterWithEntries<K, V>,
+  b: GetterWithEntries<K, any>
+): GetterWithEntries<K, V> {
+  const get = (input?: K) => {
+    if (input === undefined) {
+      return function*() {
+        const iterator = a()[Symbol.iterator]();
+
+        while (true) {
+          let item = iterator.next();
+          while (!item.done && !b(item.value[0])) {
+            item = iterator.next();
+          }
+
+          if (item.done) {
+            return;
+          }
+
+          yield item.value;
+        }
+      }();
+    } else {
+      const aResult = a(input);
+      const bResult = b(input);
+      if (!!aResult && !!bResult) {
+        return aResult;
+      }
+      return a(null);
+    }
+  };
+
+  return get as GetterWithEntries<K, V>;
+}
+
+// export function intersection<K, V>(
+//   a: GetterWithEntries<K, V>,
+//   b: GetterWithEntries<K, any>
+// ): GetterWithEntries<K, V> {
+//   return difference(a, difference(a, b));
+// }
